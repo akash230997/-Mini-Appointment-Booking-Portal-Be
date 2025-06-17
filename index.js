0656153f52063d385541e4fa04aaef319152d473
Akash Meena <akash.meena23@gmail.com>
2025-06-18 04:22:52 +0530

    Remove Mongoose Warnings

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 // Connect MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.log("❌ MongoDB error:", err));
